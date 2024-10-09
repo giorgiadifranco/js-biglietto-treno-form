4d4f3bbea4be314360059ea22b40bdfdce84fd80
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-09 16:01:10 +0200

    add value member ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,43 @@ console.log('train ticket');
 const formEl = document.querySelector('.container')
 console.log(formEl);
 
+const ticketEl = document.querySelector('.ticket');
+
+
 formEl.addEventListener('submit', (e)=>{
     e.preventDefault()
-    console.log(e.target.memberDistance.value);
+    //console.log(e.target.memberDistance.value, e.target.memberAge.value);
+
+    //creo le const
+
+    const memberDistance = e.target.memberDistance.value;
+    const memberAge = e.target.memberAge.value;
+    const memberTicket = memberDistance*0.21;
+    console.log(memberDistance, memberAge, memberTicket);
+
+    if( memberAge < 18){
+        let userTicket = memberDistance/100*20;
+        console.log(userTicket);
+        
+    
+    }
+    
+   
+
+    const memberTicketEl = ` <div class="row mb-3">
+            <label for="inputnumber" class="age col-sm-2 col-form-label" name="memberTicket">Costo del biglietto</label>
+            <div class="col-sm-10">
+              <input type="number" class="ticket form-control" id="inputAge">
+            </div>
+          </div>` 
+
     
 
-    if(age < 18){
-        0.21
-    }
+    
+
+    
+
+    
 })
 /*
 const distanceEl = document.querySelector(".distance");
