4a2933b83bbb373272e2f855e4b867331713680d
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-09 14:56:43 +0200

    fix form

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+console.log('train ticket');
+
+const distanceEl = document.querySelector(".distance");
+const ageEl = document.querySelector(".age");
+
+const btnEl = document.querySelector("btn");
+
+const distance = field.value;
+const age = field.value;
+
+btnEl.addEventListener('submit', ()=>{
+
+    if(age < 18){
+        0.21
+    }
+})
